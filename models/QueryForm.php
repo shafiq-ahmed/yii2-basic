7f61679c7d4bb8ac985e1e8b613b834c3ddf4ab2
shafiq-ahmed <shafiqahmd3@gmail.com>
2023-08-06 14:32:55 +0600

    Input validation complete
    Validation for all inputs have been added

--- models/QueryForm.php
+++ models/QueryForm.php
@@ -11,9 +11,16 @@ class QueryForm extends ActiveRecord
      return [
          // name, phone number and query are required
          [['name', 'phone_number',  'query'], 'required'],
+         //name can be at least of 3 character and max 50 characters. The upper limit
+         //is taken from the database
+         ['name','string','length'=>[3,50]],
          // email has to be a valid email address
          ['email', 'email'],
+         //subject can be empty or max 80 in length. subject will take a string input. Max length is taken from database
+         ['subject','string','length'=>[80]],
          // verifyCode needs to be entered correctly
+         //ToDo
+         //Phone numbers either 11 or 13 digits of length have to be accepted
          ['phone_number','match', 'pattern'=>'/^\d{13}$/'],
      ];
  }
